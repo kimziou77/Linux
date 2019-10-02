1a7bd9c0be1f8cfefdf7966e5d6967c38fb6517e
kimsubin <kimziou77@naver.com>
2019-10-01 21:56:28 -0700

    Add HW4 and reading DS_project file

---
+++ homework/HW04/HW04/myread.cpp
@@ -0,0 +1,22 @@
+/*
+ * Student ID: (자신의 학번을 입력하세요.)
+ * Name: (자신의 이름을 입력하세요.)
+ */
+#include "MyStudent.hpp"
+#include <fcntl.h>
+#include <iostream>
+#include <list>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(void) {
+    /* 아래에 코드를 작성하세요. */
+    list<Student> stuList;
+    list<Student>::iterator iter;
+
+    return 0;
+}
