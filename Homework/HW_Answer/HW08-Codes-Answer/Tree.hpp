c352abba18ac8e2efda22a5a841e17bb18e28bbe
kimyo <kimziou77@naver.com>
2019-12-03 03:37:55 +0900

    Add Homework answer
    HW07 & HW08

---
+++ Homework/HW_Answer/HW08-Codes-Answer/Tree.hpp
@@ -0,0 +1,24 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+
+#include "TreeNode.hpp"
+
+class Tree {
+  public:
+    Tree();
+    Tree(int data);
+    ~Tree();
+
+    void insert(int data);
+    void display();
+    bool search(int data);
+    bool remove(int data);
+
+  private:
+    void display(TreeNode *curNode);
+    bool search(TreeNode *curNode, int data);
+    bool remove(TreeNode *preNode, TreeNode *curNode, int data);
+    TreeNode *find_max_node(TreeNode *preNode, TreeNode *curNode);
+    TreeNode *rootnode;
+};
+#endif
