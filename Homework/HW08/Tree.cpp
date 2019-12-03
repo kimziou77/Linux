03cd6f5025696731f5cf8c51c67c909296deef57
kimyo <kimziou77@naver.com>
2019-12-03 13:26:18 +0900

    Revert "Add HW08 rotated"
    This reverts commit a303187c961e1fb98e68c43112521f90487c040b.

--- Homework/HW08/Tree.cpp
+++ Homework/HW08/Tree.cpp
@@ -2,189 +2,161 @@
 #include "TreeNode.hpp"
 #include <iostream>
 #include <stddef.h>
-
 Tree::Tree() { this->rootnode = NULL; }
 Tree::Tree(int data) {
-    TreeNode *node = new TreeNode(data);
-    this->rootnode = node;
-}
-Tree::~Tree() {
-    delete rootnode->getLeft();
-    delete rootnode->getRight();
-    delete rootnode;
+	TreeNode* node = new TreeNode(data);
+	this->rootnode = node;
 }
 void Tree::insert(int data) {
-    TreeNode *node = new TreeNode(data);
-    TreeNode *cursor = this->rootnode;
-    if (cursor == NULL) {
-        this->rootnode = node;
-        return;
-    }
-    while (1) {
-        int value = cursor->getData();
-        if (value < data) {
-            if (cursor->getRight() == NULL) {
-                cursor->setRight(node);
-                break;
-            } else {
-                cursor = cursor->getRight();
-            }
-        } else {
-            if (cursor->getLeft() == NULL) {
-                cursor->setLeft(node);
-                break;
-            } else {
-                cursor = cursor->getLeft();
-            }
-        }
-    }
-    return;
+	TreeNode* node = new TreeNode(data);
+	TreeNode* cursor = this->rootnode;
+	if (cursor == NULL) {
+		this->rootnode = node;
+		return;
+	}
+	while (1) {
+		int value = cursor->getData();
+		if (value < data) {
+			if (cursor->getRight() == NULL) {
+				cursor->setRight(node);
+				break;
+			}
+			else {
+				cursor = cursor->getRight();
+			}
+		}
+		else {
+			if (cursor->getLeft() == NULL) {
+				cursor->setLeft(node);
+				break;
+			}
+			else {
+				cursor = cursor->getLeft();
+			}
+		}
+	}
+	return;
 }
 void Tree::display() {
-    display(this->rootnode, 0);
-    return;
+	display(this->rootnode);
+	return;
 }
-void Tree::display(TreeNode *curNode, int depth) {
-    int i;
-    if (curNode == NULL)
-        return;
-    display(curNode->getRight(), depth + 1);
-    for (i = 0; i < depth; i++) {
-        std::cout << "   ";
-    }
-    std::cout << curNode->getData() << std::endl;
-    display(curNode->getLeft(), depth + 1);
-    return;
+void Tree::display(TreeNode* curNode) {
+	if (curNode == NULL)
+		return;
+	if (curNode->getData() == NULL)
+		return;
+	display(curNode->getLeft());
+	std::cout << " ";
+	std::cout << curNode->getData();
+	display(curNode->getRight());
+
+	return;
+}
+bool Tree::Nsearch(int data) {
+	TreeNode* tmp = this->rootnode;
+	while (1) {
+		if (tmp == NULL)
+			return false;
+		if (tmp->getData() == data)
+			return true;
+		else if (tmp->getData() > data) {
+			tmp = tmp->getLeft();
+			continue;
+		}
+		else if (tmp->getData() < data) {
+			tmp = tmp->getRight();
+			continue;
+		}
+	}
 }
 bool Tree::search(int data) {
-    bool result;
-    result = search(this->rootnode, data);
-    return result;
-}
-bool Tree::search(TreeNode *curNode, int data) {
-    if (curNode == NULL) {
-        std::cout << std::endl;
-        std::cout << data << " search failed" << std::endl;
-        return false;
-    }
-    if (curNode->getData() == data) {
-        std::cout << " " << curNode->getData() << std::endl;
-        std::cout << data << " search success!" << std::endl;
-        return true;
-    }
-    if (curNode->getData() > data) {
-        std::cout << " " << curNode->getData();
-        return search(curNode->getLeft(), data);
-    } else {
-        std::cout << " " << curNode->getData();
-        return search(curNode->getRight(), data);
-    }
-}
-bool Tree::remove(
-    int data) { // public의 remove가 호출되면 private의 remove를 호출
-    bool result;
-    result = remove(NULL, this->rootnode, data);
-    return result;
-}
-bool Tree::remove(TreeNode *preNode, TreeNode *curNode, int data) {
-    if (curNode == NULL) { // 삭제할 노드를 찾지 못한 경우
-        std::cout << std::endl;
-        std::cout << data << " remove failed" << std::endl;
-        return false;
-    }
-    TreeNode *left = curNode->getLeft();
-    TreeNode *right = curNode->getRight();
-    if (curNode->getData() == data) { // 삭제할 노드를 찾은 경우
-        if (preNode == NULL) {        // 루트 노드인 경우
-            if (left == NULL &&
-                right == NULL) { // 좌측과 우측 노드가 모두 없는 경우
-                this->rootnode = NULL;
-            } else if (left == NULL && right != NULL) { // 우측 노드만 있는 경우
-                this->rootnode = right;
-            } else if (left != NULL && right == NULL) { // 좌측 노드만 있는 경우
-                this->rootnode = left;
-            } else { // 좌측 노드와 우측 노드가 모두 있는 경우
-                TreeNode *maxNode = find_max_node(
-                    curNode, left); // 좌측 노드에서 가장 큰 노드를 찾는 함수
-                maxNode->setLeft(curNode->getLeft());
-                maxNode->setRight(curNode->getRight());
-                this->rootnode = maxNode;
-            }
-        } else { // 루트 노드가 아닌 경우
-            if (left == NULL &&
-                right == NULL) { // 좌측과 우측 노드가 모두 없는 경우
-                if (preNode->getLeft() == curNode) {
-                    preNode->setLeft(NULL);
-                } else {
-                    preNode->setRight(NULL);
-                }
-            } else if (left == NULL && right != NULL) { // 우측 노드만 있는 경우
-                if (preNode->getLeft() == curNode) {
-                    preNode->setLeft(right);
-                } else {
-                    preNode->setRight(right);
-                }
+	TreeNode* tmp = this->rootnode;
+	while (1) {
+		if (tmp == NULL) {
+			std::cout << std::endl << data << " search failed\n";
+			return false;
+		}
+		std::cout << tmp->getData() << " ";
 
-            } else if (left != NULL && right == NULL) { // 좌측 노드만 있는 경우
-                if (preNode->getLeft() == curNode) {
-                    preNode->setLeft(left);
-                } else {
-                    preNode->setRight(left);
-                }
-            } else { // 좌측 노드와 우측 노드가 모두 있는 경우
-                TreeNode *maxNode = find_max_node(
-                    curNode, left); // 좌측 노드에서 가장 큰 노드를 찾는 함수
-                maxNode->setLeft(curNode->getLeft());
-                maxNode->setRight(curNode->getRight());
-                if (preNode->getLeft() == curNode) {
-                    preNode->setLeft(maxNode);
-                } else {
-                    preNode->setRight(maxNode);
-                }
-            }
-        }
-        delete curNode;
-        std::cout << data << " remove success!" << std::endl;
-        return true;
-    } else { // 탐색하는 노드가 삭제하려는 노드가 아닌 경우
-        if (curNode->getData() > data) {
-            return remove(curNode, left, data);
-        } else {
-            return remove(curNode, right, data);
-        }
-    }
+		if (tmp->getData() == data) {
+			std::cout << std::endl << data << " search success!\n";
+			return true;
+		}
+		else if (tmp->getData() > data) {
+			tmp = tmp->getLeft();
+			continue;
+		}
+		else if (tmp->getData() < data) {
+			tmp = tmp->getRight();
+			continue;
+		}
+	}
 }
-TreeNode *Tree::find_max_node(TreeNode *preNode, TreeNode *curNode) {
-    TreeNode *pre = preNode;
-    TreeNode *cur = curNode;
-    TreeNode *left;
-    TreeNode *right;
-    while (1) {
-        left = cur->getLeft();
-        right = cur->getRight();
-        if (right ==
-            NULL) { // 탐색하는 노드에 우측 노드가 더이상 없는 경우 가장 큰 노드
-            if (left == NULL) { // 가장 큰 노드의 좌측 노드가 없는 경우
-                if (pre->getLeft() == cur) {
-                    pre->setLeft(NULL);
-                } else {
-                    pre->setRight(NULL);
-                }
-            } else { // 가장 큰 노드의 좌측 노드가 있는 경우
-                if (pre->getLeft() == cur) {
-                    pre->setLeft(cur->getLeft());
-                } else {
-                    pre->setRight(cur->getLeft());
-                }
-            }
-            return cur;
-        }
-        if (right == NULL) {
-            pre = cur;
-            cur = left;
-        } else {
-            pre = cur;
-            cur = right;
-        }
-    }
+TreeNode* Tree::dnode(int data) {
+	TreeNode* tmp = this->rootnode;
+	TreeNode* prev = tmp;
+	while (1) {
+		if (tmp == NULL) {
+			return NULL;
+		}
+		if (tmp->getData() == data) {
+			return tmp;
+		}
+		else if (tmp->getData() > data) {
+			prev = tmp;
+			tmp = tmp->getLeft();
+			continue;
+		}
+		else if (tmp->getData() < data) {
+			prev = tmp;
+			tmp = tmp->getRight();
+			continue;
+		}
+	}
+
+}
+void Tree::remove(int data) {
+
+	if (Nsearch(data)) {
+		TreeNode* data_node = dnode(data);
+		if (data_node->getLeft()) {
+			TreeNode* max_node = find_Max(data_node->getLeft());
+			data_node->setData(max_node->getData());
+			TreeNode* tmp = max_node;
+			max_node = max_node->getLeft();
+			data_node->setLeft(max_node);
+			delete tmp;
+			tmp = NULL;
+		}
+		else if(data_node->getRight()) {
+			//left가없고 Right는 있을때
+			TreeNode* origin = data_node;
+			TreeNode * next = data_node->getRight();
+			origin->setData(next->getData());
+			origin->setLeft(next->getLeft());
+			origin->setRight(next->getRight());
+			delete next;
+		}
+		else {//left도없고 Right도 없을때
+			data_node->setData(NULL);
+			data_node->setLeft(NULL);
+			data_node->setRight(NULL);
+
+		}
+		std::cout << data << " remove success!\n";
+	}
+	else {
+		std::cout << data << " remove failed\n";
+	}
+}
+TreeNode* Tree::find_Max(TreeNode* cur_node) {
+	TreeNode* tmp = cur_node;
+	TreeNode* prev = tmp;
+
+	while (tmp->getRight()) {
+		prev = tmp;
+		tmp = tmp->getRight();
+	}
+	return prev;
 }
--- Homework/HW08/Tree.hpp
+++ Homework/HW08/Tree.hpp
@@ -1,24 +1,20 @@
+#pragma once
 #ifndef __TREE_H__
 #define __TREE_H__
-
 #include "TreeNode.hpp"
-
 class Tree {
-  public:
-    Tree();
-    Tree(int data);
-    ~Tree();
-
-    void insert(int data);
-    void display();
-    bool search(int data);
-    bool remove(int data);
-
-  private:
-    void display(TreeNode *curNode, int depth);
-    bool search(TreeNode *curNode, int data);
-    bool remove(TreeNode *preNode, TreeNode *curNode, int data);
-    TreeNode *find_max_node(TreeNode *preNode, TreeNode *curNode);
-    TreeNode *rootnode;
+public:
+	Tree();
+	Tree(int data);
+	void insert(int data);
+	void remove(int data);
+	void display();
+	bool search(int data);
+	TreeNode* find_Max(TreeNode* root_node);
+private:
+	bool Nsearch(int data);
+	TreeNode* dnode(int data);
+	void display(TreeNode* curNode);
+	TreeNode* rootnode;
 };
 #endif
