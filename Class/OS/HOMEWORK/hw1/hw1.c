f9fb23ddcbdbf16ed9f21e2952e3e4dee699898c
kimziou77 <kimziou77@naver.com>
2020-03-25 20:04:37 -0700

    ADD : hw1 origin file

---
+++ Class/OS/HOMEWORK/hw1/hw1.c
@@ -0,0 +1,30 @@
+#include "hw1.h"
+
+void Init(){
+
+}
+
+void InsertObjectToTail(Object* pObj, int ObjNum){
+
+}
+
+void InsertObjectToHead(Object* pObj, int objNum){
+
+}
+
+Object* GetObjectByNum(int objnum){    
+
+}
+
+Object* GetObjectFromObjFreeList(){
+
+}
+
+BOOL DeleteObject(Object* pObj){
+
+}
+
+void InsertObjectIntoObjFreeList(Object* pObj){
+
+}
+
